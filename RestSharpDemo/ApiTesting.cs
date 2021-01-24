69331af5bf67ed979f87b3cc5755cf7d41581056
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-01-24 16:15:15 +0300

    Add project files.

---
+++ RestSharpDemo/ApiTesting.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using RestSharpDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestSharpDemo
+{
+    [TestFixture]
+    public class ApiTesting
+    {
+        readonly RestClient restClient;
+        public ApiTesting()
+        {
+            restClient = new RestClient("http://localhost:3000/");
+        }
+
+        [Test]
+        public void GetTestMethod()
+        {
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid", 1);
+
+            var response = restClient.Execute(request);
+
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<object, object>>(response);
+            //var result = output["author"];
+
+            var responseBody = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+            var result = responseBody["author"];
+
+            Assert.That(result, Is.EqualTo("typicode"), "Author is not correct");
+        }
+
+        [Test]
+        [Obsolete]
+        public void PostWithAnonymousBody()
+        {
+            RestRequest request = new RestRequest("posts/{postid}/profile", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddBody(new { name = "Yunus" });
+
+            request.AddUrlSegment("postid", 1);
+
+            var response = restClient.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var result = output["name"];
+
+            Assert.That(result, Is.EqualTo("Yunus"), "Author is not correct");
+        }
+
+        [Test]
+        [Obsolete]
+        public void PostWithTypeClassBody()
+        {
+            RestRequest request = new RestRequest("posts", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddBody(new Posts { id = "16", author = "Execute Automation", title = "RestSharp Demo" });
+
+            var response = restClient.Execute<Posts>(request);
+
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["author"];
+
+            Assert.That(response.Data.author, Is.EqualTo("Execute Automation"), "Author is not correct");
+        }
+
+        [Test]
+        [Obsolete]
+        public void PostWithAsync()
+        {
+            RestRequest request = new RestRequest("posts", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddBody(new Posts { id = "18", author = "Execute Automation", title = "RestSharp Demo" });
+
+            //var response = client.Execute<Posts>(request);
+
+            var response = ExecuteAsyncRequest<Posts>(restClient, request).GetAwaiter().GetResult();
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["author"];
+
+            Assert.That(response.Data.author, Is.EqualTo("Execute Automation"), "Author is not correct");
+        }
+
+        [Obsolete]
+        private async Task<IRestResponse<T>> ExecuteAsyncRequest<T>(RestClient client, IRestRequest request) where T : class, new()
+        {
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse<T>>();
+            client.ExecuteAsync<T>(request, restResponse =>
+            {
+                if (restResponse.ErrorException != null)
+                {
+                    const string message = "Error retrieving response.";
+                    throw new ApplicationException(message, restResponse.ErrorException);
+                }
+                taskCompletionSource.SetResult(restResponse);
+            });
+
+            return await taskCompletionSource.Task;
+        }
+    }
+}
---
+++ RestSharpDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1db059ed-48c6-434d-9559-1f42641df3e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
